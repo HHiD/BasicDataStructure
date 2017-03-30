400805b996ba263524802470ca37db906a8d5f4e
hongdi@rrkd.cn <hongdi@rrkd.cn>
2017-03-30 14:29:07 +0800

    Comment added

--- TestingProjectCBasis/TestingProjectCBasis/BitMap/BitMapHandle.h
+++ TestingProjectCBasis/TestingProjectCBasis/BitMap/BitMapHandle.h
@@ -21,14 +21,38 @@
 #include <stdbool.h>
 
 
+/**
+ Generate continuly and nondulplicate numbers
+
+ @param count from 0 ~ cout
+ @return number arrays
+ */
 int* generateNumbers(unsigned int count);
 
+/**
+ Check if this number in this bitSet existed
+
+ @param bitSet the bitSet
+ @param number the number
+ @return yes or no
+ */
 bool checkNumber(int bitSet[], int number);
 
+/**
+ Set number to bitmap(bitarray)
+
+ @param A the bitmap
+ @param number number needs to set
+ */
 void setInteger(int A[], int number);
 
-int getBitCount(unsigned int count);
+/**
+ To query a array of numbers. NOTE: this array must be continue number array
 
+ @param A number's array
+ @param count the count of this array
+ @return queried array
+ */
 int* queryIntSet(int* A, int count);
 
 #endif /* BitMapHandle_h */
